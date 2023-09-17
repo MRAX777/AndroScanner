426a5af24eb8691df01c4855b5702c7870a056e5
MRAX777 <142186754+MRAX777@users.noreply.github.com>
2023-09-16 17:46:48 -0700

    Create androscanner.php

---
+++ androscanner.php
@@ -0,0 +1,32 @@
+<?php
+function scanr()
+{
+    $Directory = new RecursiveDirectoryIterator("/home/ax/MyAndroid/");
+    $Iterator = new RecursiveIteratorIterator($Directory);
+    $Regex = new RegexIterator(
+        $Iterator,
+        '/^.+\.(mp4|jpeg|jpg|gif|webp|avif|png)$/i',
+        RecursiveRegexIterator::GET_MATCH
+    );
+    foreach ($Regex as $r) {
+        $f = str_replace("/home/ax/MyAndroid/", "/var/www/vids/temp/", $r[0]);
+        @mkdir(dirname($f), 0777, true);
+        echo "$r[0] > $f ";
+        if (!file_exists($f) or filesize($r[0]) > filesize($f)) {
+            copy($r[0], $f);
+        }
+        if (filesize($r[0]) == filesize($f)) {
+            unlink($r[0]);
+        }
+        echo " Moved!\n";
+        sleep(1);
+    }
+}
+function __destruct()
+{
+    shell_exec("fusermount -u /home/ax/MyAndroid");
+    shell_exec("go-mtpfs /home/ax/MyAndroid");
+    shell_exec("php /home/ax/andscanner.php");
+}
+
+scanr();
